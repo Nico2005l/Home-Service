4350ab2f34c6ba56126db06981bc9edbbf1b5321
Tomas Liñeira Abal <tlineira@gmail.com>
2025-06-22 19:14:55 -0300

    agregado de opciones de ordenamiento en MarketPage.jsx

--- src/components/MarketPage.jsx
+++ src/components/MarketPage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 
-const CATEGORIES = ["Plomería", "Electricista", "Limpieza"];
+const CATEGORIES = ["Plomería", "Electricista", "Limpieza", "Jardinería", "Otros"];
 
 const MarketPage = () => {
   const [services, setServices] = useState([]);
